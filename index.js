17f41531747f7ac68fcd4e581d718dbbd4d3e1ad
sohaghosen <sohag.hosen@outlook.com>
2021-10-29 15:48:35 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+
 const app = express();
+
+// middleware
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.sz6ue.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db(process.env.DB_NAME);
+    const hotels = database.collection("hotels");
+    app.get("/hotels", async (req, res) => {
+      const result = await hotels.find({}).toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
